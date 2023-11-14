3badd5c1b28f74eb523c2e59d0663e8d1ddfc299
vashaferov <jonirz55@gmail.com>
2023-11-14 22:08:53 +0500

    tmp_1

--- TMS_AQA/Program.cs
+++ TMS_AQA/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+const int MINUTS_IN_HOUER = 60;
+string name;
+
+Console.Write("Hello, World! Hi, ");
+name = "Bob";
+Console.WriteLine(name + ".");
+
+// Подготовка данных
+
+bool isWhiteBreadFresh = true;
+ushort whiteBreadPrice = 50;
+
+ushort butterPrice = 70;
+
+ushort milkPrice = 60;
+float milkFatPrec = 1.2F;
