fa804975d84ab8fb5fcae3345b742b7c9d35cb84
vashaferov <jonirz55@gmail.com>
2023-11-16 21:40:21 +0500

    finish TypesAndOperators

--- TMS_AQA/Program.cs
+++ TMS_AQA/Program.cs
@@ -11,8 +11,50 @@ Console.WriteLine(name + ".");
 
 bool isWhiteBreadFresh = true;
 ushort whiteBreadPrice = 50;
-
 ushort butterPrice = 70;
-
 ushort milkPrice = 60;
 float milkFatPrec = 1.2F;
+ushort icecreaPrec = 250;
+
+Console.Write("Ввелите начальную сумму: ");
+string? sumString = Console.ReadLine();
+short sum = Convert.ToInt16(sumString);
+
+string slip = "ЧЕК:\n";
+
+if (sum > 0)
+{
+    if (isWhiteBreadFresh)
+        if (sum >= whiteBreadPrice)
+        {
+            sum -= (short)whiteBreadPrice;
+            slip += $"Батон - {whiteBreadPrice}\n";
+        }
+    else
+        Console.WriteLine("Батон не свежый");
+
+    if (sum >= butterPrice)
+    {
+        sum -= (short)butterPrice;
+        slip += $"Масло - {butterPrice}\n";
+    }
+
+    if (milkFatPrec == 1.2F)
+    {
+        if (sum >= milkPrice)
+        {
+            sum -= (short)milkPrice;
+            slip += $"Молоко - {milkPrice}\n";
+        }
+    }    
+    else
+        Console.WriteLine("Молоко не той жирности");
+
+    if (sum >= icecreaPrec)
+        Console.WriteLine($"{slip}Мороженое - {icecreaPrec}\nИтого: {(short)(sum-icecreaPrec)}"); 
+    else
+        Console.WriteLine($"{slip}\nИтого: {sum}");
+}
+else
+    Console.WriteLine("Не дали ничего(");
+
