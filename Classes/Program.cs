b15616ddf8a0373e6518a65bc7f415fb9d858029
vashaferov <jonirz55@gmail.com>
2023-11-28 22:24:39 +0500

    Создание бранча "Classes_Homework"

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,123 +6,125 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-              //=================================\\
-             //============= Методы ==============\\
+            //=================================\\
+            //============= Методы ==============\\
             //=====================================\\
-           /* int[] array = { 1, 8, 14, -4, 0, 7 };
+            /* int[] array = { 1, 8, 14, -4, 0, 7 };
 
-            DisplayGreeting("Hello");
-            DisplayGreeting("Hello", "Slava");
+             DisplayGreeting("Hello");
+             DisplayGreeting("Hello", "Slava");
 
-            Console.WriteLine(GetInfo());
+             Console.WriteLine(GetInfo());
 
-            PrintArrayWithoutParameters();
+             PrintArrayWithoutParameters();
 
-            Array.Reverse(array);
+             Array.Reverse(array);
 
-            PrintArrayParameters(array);
+             PrintArrayParameters(array);
 
 
-            void PrintArrayWithoutParameters()
-            {
-                foreach (var i in array)
-                {
-                    Console.Write($"{i}\t");
-                }
-                Console.WriteLine();
-            }
+             void PrintArrayWithoutParameters()
+             {
+                 foreach (var i in array)
+                 {
+                     Console.Write($"{i}\t");
+                 }
+                 Console.WriteLine();
+             }
 
-            void PrintArrayParameters(int[] arrayParameters)
-            {
-                foreach (var i in arrayParameters)
-                {
-                    Console.Write($"{i}\t");
-                }
-                Console.WriteLine();
-            }
+             void PrintArrayParameters(int[] arrayParameters)
+             {
+                 foreach (var i in arrayParameters)
+                 {
+                     Console.Write($"{i}\t");
+                 }
+                 Console.WriteLine();
+             }
 
-            string GetInfo()
-            {
-                return "Reverse:";
-            }
+             string GetInfo()
+             {
+                 return "Reverse:";
+             }
 
-            void DisplayGreeting(string message, string name = "Гость") // с параметром по умолчанию
-            {
-                Console.WriteLine(message + ", " + name);
-            }
+             void DisplayGreeting(string message, string name = "Гость") // с параметром по умолчанию
+             {
+                 Console.WriteLine(message + ", " + name);
+             }
 
-            // =============== Передача параметров по ссылке
+             // =============== Передача параметров по ссылке
 
-            static void OutAdd (int x, int y, out int ans)              // out
-            {
-                ans = x + y;
-            }
+             static void OutAdd (int x, int y, out int ans)              // out
+             {
+                 ans = x + y;
+             }
 
-            OutAdd(90, 90, out int ans);                                // ref
-            Console.WriteLine("Out: " + ans);
+             OutAdd(90, 90, out int ans);                                // ref
+             Console.WriteLine("Out: " + ans);
 
-            void ModifyValue(ref int value)
-            {
-                value = value * 2;
-            }
+             void ModifyValue(ref int value)
+             {
+                 value = value * 2;
+             }
 
-            int mainValue = 5;
-            Console.WriteLine("Ref: " + mainValue);
+             int mainValue = 5;
+             Console.WriteLine("Ref: " + mainValue);
 
-            // =============== Возврат нескольких значений (кортеж)
+             // =============== Возврат нескольких значений (кортеж)
 
-            (string, int) GetPersonInfo()
-            {
-                return ("Slava", 25);
-            }
+             (string, int) GetPersonInfo()
+             {
+                 return ("Slava", 25);
+             }
 
-            var person1 = GetPersonInfo();
-            Console.WriteLine($"Name: {person1.Item1}, Age: {person1.Item2}");
+             var person1 = GetPersonInfo();
+             Console.WriteLine($"Name: {person1.Item1}, Age: {person1.Item2}");
 
-            // =============== Рекурсия
+             // =============== Рекурсия
 
-            int Factorial(int n)
-            {
-                if (n == 1) return 1;
+             int Factorial(int n)
+             {
+                 if (n == 1) return 1;
 
-                return n * Factorial(n - 1);
-            }
+                 return n * Factorial(n - 1);
+             }
 
-            int factorial4 = Factorial(4);*/
-    
-              //=================================\\
-             //============= Классы ==============\\
-            //=====================================\\
+             int factorial4 = Factorial(4);
 
-            EmptyClass emptyClass = new EmptyClass();
-            Person person = new Person();
+               //=================================\\
+              //============= Классы ==============\\
+             //=====================================\\
 
-            person.Print();
+             EmptyClass emptyClass = new EmptyClass();
+             Person person = new Person();
 
-            person.name = "Slava";
-            person.age = 25;
-            person.Print();
+             person.Print();
 
-            // =============== Конструктор
+             person.name = "Slava";
+             person.age = 25;
+             person.Print();
 
-            PersonWithConstructor personWithConstructor = new PersonWithConstructor();
-            personWithConstructor.Print();
+             // =============== Конструктор
 
-            CustomConstructor customConstructor1 = new CustomConstructor(10);
-            CustomConstructor customConstructor2 = new CustomConstructor("text");
-            customConstructor1 = new CustomConstructor("newText");
+             PersonWithConstructor personWithConstructor = new PersonWithConstructor();
+             personWithConstructor.Print();
 
-            PersonThis personThis = new PersonThis();
+             CustomConstructor customConstructor1 = new CustomConstructor(10);
+             CustomConstructor customConstructor2 = new CustomConstructor("text");
+             customConstructor1 = new CustomConstructor("newText");
 
-            // Цепочки
+             PersonThis personThis = new PersonThis();
 
-            PersonChain personChain1 = new PersonChain();
-            PersonChain personChain2 = new PersonChain("Slava");
-            PersonChain personChain3 = new PersonChain("Slava", 25);
+             // Цепочки
 
-            personChain1.Print();
-            personChain2.Print();
-            personChain3.Print();
+             PersonChain personChain1 = new PersonChain();
+             PersonChain personChain2 = new PersonChain("Slava");
+             PersonChain personChain3 = new PersonChain("Slava", 25);
+
+             personChain1.Print();
+             personChain2.Print();
+             personChain3.Print();*/
+
+
         }
     }
 }
