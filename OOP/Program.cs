a1491f07cd371911274dd483559d9f31a8bdb551
vashaferov <jonirz55@gmail.com>
2023-11-30 22:02:53 +0500

    Лекция

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Client client1 = new Client(1);
+            client1.Id = 1;
+            client1.PhoneNumber = "79126784609";
+            client1.Email = "test@test.com";
+
+            Corporate corporate1 = new Corporate(1);
+            corporate1.Id = 1;
+            corporate1.INN = 4523434;
+            corporate1.UNP = 3234324;
+
+            Individuals individual1 = new Individuals(1);
+            individual1.Firstname = "Aaa";
+            individual1.Lastname = "Bbb";
+            individual1.Id = 10;
+            individual1.PhoneNumber = "79373892819";
+            individual1.Email = "test1@test.com";
         }
     }
 }
