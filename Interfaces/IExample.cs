c787cb7cb4988d08a89f8111b9b73565c8099605
vashaferov <jonirz55@gmail.com>
2023-12-05 22:01:21 +0500

    Интерфейсы_1

---
+++ Interfaces/IExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal interface IExample
+    {
+        const int minSpeed = 0;
+        static int maxSpeed = 100;
+
+        void Move();
+
+        string Name { get; set; }
+    }
+}
