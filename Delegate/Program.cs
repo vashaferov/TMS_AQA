9add741c4b06393b625ad1c956b493346ad4edbc
vashaferov <jonirz55@gmail.com>
2024-01-13 14:07:14 +0500

    Промежуточный

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Delegate;
 
 //new Example1().Run();
 
@@ -7,4 +6,50 @@ using Delegate;
 
 //new Example3().Run();
 
-new Example4().Run();
+//new Example4().Run();
+
+bool flag = true;
+string task;
+
+while (flag)
+{
+    Console.Write("-------\nВведите номер задания (от 1 до 4): ");
+    task = Console.ReadLine();
+    Console.Clear();
+
+    switch (task)
+    {
+        case "1":
+            {
+                Console.WriteLine("ЗАДАНИЕ 1\n");
+
+                
+
+                break;
+            }
+        case "2":
+            {
+                Console.WriteLine("ЗАДАНИЕ 2\n");
+               
+                break;
+            }
+        case "3":
+            {
+                Console.WriteLine("ЗАДАНИЕ 3\n");
+
+                break;
+            }
+        case "4":
+            {
+                Console.WriteLine("ЗАДАНИЕ 4\n");
+
+                break;
+            }
+        default:
+            {
+                Console.WriteLine("Не найден номер задачи. Повторите ввод номера (от 1 до 4)");
+
+                break;
+            }
+    }
+}
---
+++ Delegate/Task1/RandomD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate.Task1
+{
+    internal class RandomD
+    {
+        public delegate int Randoms();
+    }
+}
