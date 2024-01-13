f4882c6c07b6100be74d349d9c18ef5da434baca
vashaferov <jonirz55@gmail.com>
2024-01-13 15:57:17 +0500

    лекция

---
+++ LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LINQ;
+using System.Net.Http.Headers;
+
+//TestTask testTask= new TestTask();
+//testTask.Solve();
+
+//BaseSyntax baseSyntax= new BaseSyntax();
+//baseSyntax.RunQuerySyntax();
+//baseSyntax.RunMethodSyntax();
+
+//new Where().RunQuerySyntax();
+//new Where().RunMethodSyntax();
+
+//new Select().RunQuerySyntax();
+//new Select().RunMethodSyntax();
+
+new TwoInOne().run();
