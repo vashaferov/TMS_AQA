aa84a41ac26fb54c836cc3432ba973fa72b3d252
vashaferov <jonirz55@gmail.com>
2024-01-23 00:36:13 +0500

    HW 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -23,4 +23,63 @@ using System.Net.Http.Headers;
 //new DataAggregation().RunQuerySyntax();
 //new DataAggregation().RunMethodSyntax();
 
-new OrderBy().RunQuerySyntax();
+//new OrderBy().RunQuerySyntax();
+
+bool flag = true;
+string task;
+
+while (flag)
+{
+    Console.Write("-------\nВведите номер задания (от 1 до 4): ");
+    task = Console.ReadLine();
+
+    switch (task)
+    {
+        case "1":
+            {
+                Console.WriteLine("ЗАДАНИЕ 1\n");
+
+                int[] numbers = {3, 2, 3, 6, 7, 5, 2, 9, 2, 6, 9};
+
+                Console.WriteLine("Последовательность ДО: ");
+                foreach(var num in numbers)
+                    Console.Write(num + " ");
+
+                IEnumerable<int> numQuery =
+                    (from num in numbers
+                    where num % 2 != 0 
+                    select num).Distinct();
+
+                Console.WriteLine("\nПоследовательность ПОСЛЕ: ");
+                foreach(var num in numQuery)
+                    Console.Write(num + " ");
+                Console.WriteLine();
+                
+                break;
+            }
+        case "2":
+            {
+                Console.WriteLine("ЗАДАНИЕ 2\n");
+
+                break;
+            }
+        case "3":
+            {
+                Console.WriteLine("ЗАДАНИЕ 3\n");
+
+                break;
+            }
+        case "4":
+            {
+                Console.WriteLine("ЗАДАНИЕ 4\n");
+
+                break;
+            }
+        default:
+            {
+                Console.WriteLine("Не найден номер задачи. Повторите ввод номера (от 1 до 4)");
+
+                break;
+            }
+    }
+}
