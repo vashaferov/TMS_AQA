9a229d3e3ac49453938095151dda92adfb7228bc
vashaferov <jonirz55@gmail.com>
2024-02-03 16:46:12 +0500

    HW 1

--- SeleniumAdvanced/Tests/Howmework/AdvancedTest.cs
+++ SeleniumAdvanced/Tests/Howmework/AdvancedTest.cs
@@ -1,17 +1,30 @@
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 
 namespace SeleniumAdvanced.Tests.Howmework;
 
 public class AdvancedTest : BaseTest
 {
     [Test]
-    public void ActionTest()
+    [Description("Task1. Context Menu")]
+    public void ContextMenu()
     {
         Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/ ");
         
-        // Task1. Context Menu
         Driver.FindElement(By.LinkText("Context Menu")).Click();
         
         IWebElement box = WaitsHelper.FluentWaitForElement(By.Id("hot-spot"));
+        var actions = new Actions(Driver);
+        
+        actions
+            .MoveToElement(box, 10, 10)
+            .ContextClick()
+            .Build()
+            .Perform();
+        
+        IAlert alert = Driver.SwitchTo().Alert();
+
+        Assert.That(alert.Text, Is.EqualTo("You selected a context menu"));
+        alert.Accept();
     }
 }
