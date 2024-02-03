319e7379f2e4b7b785ea0a50459f607912cd345e
vashaferov <jonirz55@gmail.com>
2024-02-03 15:49:11 +0500

    Новый бранч

---
+++ PageObjectSimple/Tests/BaseTest.cs
@@ -0,0 +1,27 @@
+using OpenQA.Selenium;
+using PageObjectSimple.Core;
+using PageObjectSimple.Helpers;
+using PageObjectSimple.Helpers.Configuration;
+
+namespace PageObjectSimple.Tests;
+
+[Parallelizable(scope: ParallelScope.All)]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class BaseTest
+{
+    protected IWebDriver Driver { get; private set; }
+    protected WaitsHelper WaitsHelper { get; private set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        Driver = new Browser().Driver;
+        WaitsHelper = new WaitsHelper(Driver, TimeSpan.FromSeconds(Configurator.WaitsTimeout));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+}
